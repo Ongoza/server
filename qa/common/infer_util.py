fa6fbd93f69db8fe1cc6fc172ea91251f13be086
Katherine Yang <katheriney@nvidia.com>
2022-08-03 17:57:14 -0700

    simple refactor

--- qa/common/infer_util.py
+++ qa/common/infer_util.py
@@ -114,10 +114,6 @@ def infer_exact(tester,
                 use_cuda_shared_memory=False,
                 priority=0,
                 timeout_us=0):
-    # Lazy shm imports...
-    if use_system_shared_memory or use_cuda_shared_memory:
-        import tritonclient.utils.shared_memory as shm
-        import tritonclient.utils.cuda_shared_memory as cudashm
 
     tester.assertTrue(use_http or use_grpc or use_streaming)
     # configs [ url, protocol, async stream, binary data ]
@@ -154,8 +150,6 @@ def infer_exact(tester,
         np.iinfo(rinput_dtype).max,
         np.iinfo(routput0_dtype).max,
         np.iinfo(routput1_dtype).max) / 2
-
-    num_classes = 3
 
     input0_array = np.random.randint(low=val_min,
                                      high=val_max,
@@ -278,18 +272,45 @@ def infer_exact(tester,
         input0_byte_size = sum([i0.nbytes for i0 in input0_list_tmp])
         input1_byte_size = sum([i1.nbytes for i1 in input1_list_tmp])
 
-    # Create system/cuda shared memory regions if needed
-    shm_regions, shm_handles = su.create_set_shm_regions(
-        input0_list_tmp, input1_list_tmp, output0_byte_size, output1_byte_size,
-        outputs, shm_region_names, precreated_shm_regions,
-        use_system_shared_memory, use_cuda_shared_memory)
-
     if model_version is not None:
         model_version = str(model_version)
     else:
         model_version = ""
 
     # Run inference and check results for each config
+    inferAndCheckResults(
+        tester, configs, pf, batch_size, model_version, input_dtype,
+        output0_dtype, output1_dtype, INPUT0, INPUT1, OUTPUT0, OUTPUT1,
+        tensor_shape, input0_array, input1_array, output0_array,
+        output1_array, output0_raw, output1_raw, outputs,
+        precreated_shm_regions, input0_list_tmp, input1_list_tmp,
+        shm_region_names, input0_byte_size, input1_byte_size,
+        output0_byte_size, output1_byte_size, use_system_shared_memory,
+        use_cuda_shared_memory, skip_request_id_check)
+
+
+
+def inferAndCheckResults(tester, configs, pf, batch_size, model_version,
+                         input_dtype, output0_dtype, output1_dtype, INPUT0,
+                         INPUT1, OUTPUT0, OUTPUT1, tensor_shape, input0_array,
+                         input1_array, output0_array, output1_array,
+                         output0_raw, output1_raw, outputs,
+                         precreated_shm_regions, input0_list_tmp,
+                         input1_list_tmp, shm_region_names, input0_byte_size,
+                         input1_byte_size, output0_byte_size, output1_byte_size,
+                         use_system_shared_memory, use_cuda_shared_memory,
+                         skip_request_id_check):
+    # Lazy shm imports...
+    if use_system_shared_memory or use_cuda_shared_memory:
+        import tritonclient.utils.shared_memory as shm
+        import tritonclient.utils.cuda_shared_memory as cudashm
+    num_classes = 3
+
+    # Create system/cuda shared memory regions if needed
+    shm_regions, shm_handles = su.create_set_shm_regions(
+        input0_list_tmp, input1_list_tmp, output0_byte_size, output1_byte_size,
+        outputs, shm_region_names, precreated_shm_regions,
+        use_system_shared_memory, use_cuda_shared_memory)
     for config in configs:
         model_name = tu.get_model_name(pf, input_dtype, output0_dtype,
                                        output1_dtype)
